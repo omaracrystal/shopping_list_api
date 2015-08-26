4302e84549b9a6ada60c1bf821332b46147f42e0
omaracrystal <omaracrystal@gmail.com>
2015-08-26 15:19:01 -0600

    added routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,84 +1,67 @@
 var express = require('express');
 var router = express.Router();
 
+///////////////////////* GLOBAL *////////////////////////
+
+// constructor
+function ItemLibrary() {
+  this.items = [];
+  this.id = 0;
+}
+
+// methods
+ItemLibrary.prototype.addItem = function(name) {
+  var newItem = {name: name, id: this.id};
+  this.items.push(newItem);
+  this.id += 1;
+};
+
+// create some instances
+var storage = new ItemLibrary();
+storage.addItem('Noodles');
+storage.addItem('Tomatoes');
+storage.addItem('Peppers');
 
 
-////////////////////////////* PUPPY SECTION *///////////////////////////////
-
-/////////////////////GLOBAL VARIABLES////////////////////
-//create puppy array to capture input data to print in console of terminal
-var puppyArray = [];
 
 
 /////////////////////GET  ROUTES////////////////////////
-// route handler for handling a GET request to '/'
+
 router.get('/', function(req, res, next) {
-  //sends a response to the user in the form of a template called index.html
   res.render('index');
 });
 
-
-/////////////////////POST ROUTES///////////////////////
-//route handler for handling a POST request to '/submit
-//posting to server and /submit is the end point
-router.post('/submit', function(req, res, next) {
-  //8th step from todo.md
-  var puppyInputName = req.body.puppyName
-  var puppyInputID = req.body.puppyID
-  var errors = puppyValidationCheck(puppyInputName, puppyInputID)
-
-    //test for errors
-    if(errors.length > 0) {
-      res.render('index', {
-        errors: errors
-      })
-    } else {
-      //6th step from todo.md
-      puppyArray.push({
-        name: puppyInputName,
-        id: puppyInputID
-    })
-      //grabs view template and sends a response to user in the form of dog.html
-      res.render('dog', {
-        puppies: puppyArray,
-        success: "Yay! you just added a dog!"
-      })
-    }
-
-  //get data from form always req.body .puppyName (name attr from index.html)
-  console.log(req.body.puppyName)
-  console.log(req.body.puppyID)
-
-  //console.log(req.body)- in terminal> to test1
-
-  console.log(puppyArray);
-  //res.send("yo dawg"); < to test1
-
+router.get('/items', function(req, res, next) {
+  res.json(storage.items);
 });
 
 
+// /////////////////////POST ROUTES///////////////////////
 
-///////////////////////Utilities////////////////////////
+router.post('/submit', function(req, res, next) {
+  var itemInputName = req.body.itemName
+    // res.render('items', {
+    storage.addItem(itemInputName);
+    //   success: "Yay! you just added an item!"
+    // })
+    res.json(storage);
+});
 
-//right under router
-function puppyValidationCheck(puppyName, puppyID) {
-  //error array to show all errors at once
-  var errorArray = [];
-  //.trim white space
-  var puppyNameTrimmed = puppyName.trim();
-  var puppyIDTrimmed = puppyID.trim();
-  //puppy name validations
-  if(puppyNameTrimmed ==='') {
-    errorArray.push("Name cannot be blank.");
-  }
 
-  //puppy ID validations
-  if(puppyIDTrimmed === '') {
-    errorArray.push('ID cannot be blank.');
-  } else if (puppyIDTrimmed.length < 3) {
-    errorArray.push('An ID must be at least 3 characters long.');
-  }
+// /////////////////////PUT ROUTES///////////////////////
+router.put('/items/:id', function (req, res, next) {
+  // storage.items[req.params.id].name =
+});
 
-  return errorArray;
+// /////////////////////DELETE ROUTES///////////////////////
+router.delete('/items/:id', function(req, res, next) {
+  storage.items.splice([req.params.id], 1);
+  res.json(storage.items[req.params.id]);
+});
 
-}
+
+module.exports = router;
+
+
+///////////////////////* Utilities/Logic *////////////////////////
+
